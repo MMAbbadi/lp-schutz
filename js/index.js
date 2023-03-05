b3c4f2c92984f11de573fc3f8fe625f2280d30bb
MMAbbadi <mmabbadi99@gmail.com>
2023-03-05 19:56:51 -0300

    good noght

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+
+// BARRA DE BUSCA
+
+const icone = document.querySelector('.icone');
+const buscar = document.querySelector('.buscar');
+
+icone.addEventListener('click', function(){
+    buscar.classList.toggle('ativo');
+})
